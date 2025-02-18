15dacc885882e7f2eca3a57183ce3db608e71162
pawanacharya73 <pawanacharya832@gmail.com>
2025-02-19 00:17:57 +0545

    window, renderer, SDL_Log, and main loop

--- main.c
+++ main.c
@@ -4,23 +4,72 @@
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 800
 
-int main() {
-    //creating a window
-    SDL_Window *window = SDL_CreateWindow("RayTracing", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+int main()
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
 
-    // screen loop
-    int running = 1;
-    SDL_Event event;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = 0;
-            }
-        }
+    int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+    // if something unexpected happens when initializing
+    if (result < 0)
+    {
+        SDL_Log("SDL_Init Error %s", SDL_GetError());
+        return -1;
     }
 
-    // cleanup
+    window = SDL_CreateWindow("Ray Tracing", 800, 600, 0);
+    if (window == NULL)
+    {
+        SDL_Log("SDL_CreateWindow Error %s", SDL_GetError());
+        return -2;
+    }
+
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == NULL)
+    {
+        SDL_Log("SDL_CreateRenderer Error %s", SDL_GetError());
+        return -3;
+    }
+
+    SDL_Log("SDL3 initialized.");
+
+    SDL_Event event;
+    int quit = 0; // assigning a variable to quiting
+
+    // main loop
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_EVENT_QUIT:
+                SDL_Log("SDL3 Event Quit");
+                quit = 1;
+                break;
+            case SDL_EVENT_KEY_DOWN:
+                if (event.key.key == SDLK_ESCAPE)
+                {
+                    SDL_Log("SDL3 ESC key Quit");
+                    quit = 1;
+                }
+            default:
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff); // black color
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        SDL_Delay(1); // prevents CPU from being fully consumed by the game loop
+    }
+
+    // cleaning up when window is closed
+    SDL_Log("SDL3 shutdown");
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
