84a1fc651bc800b800fe834d49d50c2c9cc8564d
pawanacharya73 <pawanacharya832@gmail.com>
2025-02-19 23:52:23 +0545

    drawing a pixel on screen

--- main.c
+++ main.c
@@ -6,70 +6,30 @@
 
 int main()
 {
+   
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
+    // initializing SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    //creating window and renderer
+    SDL_CreateWindowAndRenderer("My Window",800,500, 0, &window, &renderer);
+    
+    // black color to draw the screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
+    SDL_RenderClear(renderer); //clear screen to black
 
-    int result = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // setting the draw color to white
 
-    // if something unexpected happens when initializing
-    if (result < 0)
-    {
-        SDL_Log("SDL_Init Error %s", SDL_GetError());
-        return -1;
-    }
+    // drawing a single pixel on the screen
+    SDL_RenderPoint(renderer, 800/2, 500/2);
+    
+    SDL_RenderPresent(renderer); 
 
-    window = SDL_CreateWindow("Ray Tracing", 800, 600, 0);
-    if (window == NULL)
-    {
-        SDL_Log("SDL_CreateWindow Error %s", SDL_GetError());
-        return -2;
-    }
 
-    renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == NULL)
-    {
-        SDL_Log("SDL_CreateRenderer Error %s", SDL_GetError());
-        return -3;
-    }
-
-    SDL_Log("SDL3 initialized.");
-
-    SDL_Event event;
-    int quit = 0; // assigning a variable to quiting
-
-    // main loop
-    while (!quit)
-    {
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_EVENT_QUIT:
-                SDL_Log("SDL3 Event Quit");
-                quit = 1;
-                break;
-            case SDL_EVENT_KEY_DOWN:
-                if (event.key.key == SDLK_ESCAPE)
-                {
-                    SDL_Log("SDL3 ESC key Quit");
-                    quit = 1;
-                }
-            default:
-                break;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff); // black color
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(1); // prevents CPU from being fully consumed by the game loop
-    }
-
-    // cleaning up when window is closed
-    SDL_Log("SDL3 shutdown");
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    SDL_Delay(10000); // delay of 10secs before window closes
 
     return 0;
+       
 }
+
+
