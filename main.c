d7be3dc2a0463c3e5a6344cdfc48a255789e54e7
pawanacharya73 <pawanacharya832@gmail.com>
2025-02-21 01:14:01 +0545

    drawing a circle and the sqaure around it

--- main.c
+++ main.c
@@ -1,27 +1,65 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
+#include <math.h>
 
-#define SCREEN_WIDTH 1200
-#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 800 
+#define SCREEN_HEIGHT 600
+
+struct Circle {
+    double x, y;
+    double r;
+};
+
+// a beautiful function to draw a desired circle :)
+void drawCircle(struct Circle circle,SDL_Renderer* renderer) {
+    double radius_squared = pow(circle.r, 2);
+    for (double x = circle.x - circle.r; x <= circle.x + circle.r; x++) {
+        
+        for (double y = circle.y - circle.r; y <= circle.y + circle.r; y ++) {
+            // if it satisfies the eqn of a circle
+            double distance_squared = pow(x - circle.x, 2) + pow(y - circle.y, 2);
+            if(distance_squared <= radius_squared) {
+
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderPoint(renderer, x, y);
+            }
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            // drawing the square around it
+            if((x == circle.x - circle.r || y == circle.y - circle.r) || 
+                    (x == circle.x + circle.r || y == circle.y + circle.r)){
+                SDL_RenderPoint(renderer, x, y);
+            }
+
+        }
+    }
+
+
+
+}
+
 
 int main()
 {
-   
+
     SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
+    SDL_Renderer* renderer = NULL;
     // initializing SDL
     SDL_Init(SDL_INIT_VIDEO);
     //creating window and renderer
-    SDL_CreateWindowAndRenderer("My Window",800,500, 0, &window, &renderer);
-   
-   int quit = 0;
-   SDL_Event event;
-   //main loop
+    SDL_CreateWindowAndRenderer("My Window",SCREEN_WIDTH,SCREEN_HEIGHT, 0, &window, &renderer);
+
+    // light source circle
+    struct Circle lightCircle = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 80};
+    //temp circle
+    struct Circle tempCircle = {100, 100, 100};
+    int quit = 0;
+    SDL_Event event;
+    //main loop
     while (!quit) {
         // black color to draw the screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
         SDL_RenderClear(renderer); //clear screen to black
-        
+
         while(SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_EVENT_QUIT:
@@ -29,15 +67,22 @@ int main()
                     quit = 1; 
             }
         }
-        
-
-    SDL_RenderPresent(renderer); 
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        // drawing lightCircle
+        drawCircle(lightCircle, renderer);
+        //drawing another circle just because
+        drawCircle(tempCircle, renderer);
+        // commit the renders or that kinda thing
+        SDL_RenderPresent(renderer); 
     }
 
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
 
 }
+
 
 
